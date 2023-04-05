ed82946fcd2e388e18d7b6028a59f6c3f2328fe7
Oleh109 <kadnikod@gmail.com>
2023-04-05 21:25:58 +0300

    commit (5 april - 4)

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     resolve: {
         alias: {
             img: path.resolve(__dirname, "./img"),
+            styles: path.resolve(__dirname, "./styles")
         },
     },
 });
